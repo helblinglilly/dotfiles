c56288fc752221c1db6c42080c62408e2db039b3
Lilly Helbling <lilly.helbling@onthebeach.co.uk>
2026-06-30 11:27:49 +0100

    Add file inspection on ios/android

--- mobile/generic.zsh
+++ mobile/generic.zsh
@@ -1,4 +1,13 @@
 function mobileHelp(){
+  echo "Local files:"
+  echo "  Android"
+  echo "    > adb shell"
+  echo "      run-as [app id]"
+  echo "      ls files"
+  echo "  iOS"
+  echo '    > open open "$(xcrun simctl get_app_container booted [app identifier] data)'
+  echo ""
   echo "Deeplinks:"
   echo "  > npx uri-scheme open [deeplink] --ios --android"
+  echo ""
 }
